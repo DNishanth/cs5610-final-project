0d685ad462518e7415fd92dd53059160b51b5940
Bayley Carter <carter.ba@northeastern.edu>
2022-12-14 18:00:01 -0500

    added navbar to search pages

--- src/search-page.js/index.js
+++ src/search-page.js/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 const SearchPageComponent = () => 
 
     <div className="wd-search-page">
+        <div className="row d-flex justify-content-between">
             <div className="col-6">
                 <ul className="nav nav-pills mb-2 mt-2 ms-2">
                     <h2 className="nav-item" style={{marginTop:"-11px"}}>
@@ -17,9 +18,20 @@ const SearchPageComponent = () =>
                     <li className="nav-item">
                         <a href="/profile" className="nav-link">Profile</a>
                     </li>
- 
                 </ul>
             </div>
+            <div className="col-3">
+                <ul className="nav nav-pills mb-2 mt-2 ms-5">
+                    <li className="nav-item">
+                        <a href="/login" className="nav-link">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/register" className="nav-link">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+            
         <div className="mt-5 w-75 ms-5" align="center">
             <SearchComponent/>
         </div>
--- src/search/search-results.js
+++ src/search/search-results.js
@@ -14,6 +14,34 @@ const SearchResultsComponent = () => {
     }, [searchQuery]);
     return (
         <div>
+        <div className="row d-flex justify-content-between">
+            <div className="col-6">
+                <ul className="nav nav-pills mb-2 mt-2 ms-2">
+                    <h2 className="nav-item" style={{marginTop:"-11px"}}>
+                        <a href="/home" className="nav-link">Athenaeum</a>
+                    </h2>
+                    <li className="nav-item">
+                        <a href="/home" className="nav-link ">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/search" className="nav-link active">Search</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/profile" className="nav-link">Profile</a>
+                    </li>
+                </ul>
+            </div>
+            <div className="col-3">
+                <ul className="nav nav-pills mb-2 mt-2 ms-5">
+                    <li className="nav-item">
+                        <a href="/login" className="nav-link">Login</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/register" className="nav-link">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
             <ul className="list-group">
                 <SearchBarComponent searchTerm={searchQuery}/>
                 {bookSearchResults.docs && bookSearchResults.docs.map(result => (
